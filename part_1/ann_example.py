6de74a3ce29ac0becafe1ad232afe7562c7222dc
amontemayor <amontemayor@grainchain.io>
2025-03-07 15:47:33 -0600

    added: prediction output example

--- part_1/ann_example.py
+++ part_1/ann_example.py
@@ -45,3 +45,15 @@ print("Adding the second hidden layer...")
 ann.add(tf.keras.layers.Dense(units=6, activation="relu"))
 print("Adding the output layer...")
 ann.add(tf.keras.layers.Dense(units=1, activation="sigmoid"))
+
+print("Training the ANN")
+
+print("Compiling the ANN")
+ann.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+print("Training the ANN on the Training set")
+ann.fit(X_train, y_train, batch_size=32, epochs=100)
+
+prediction = ann.predict(sc.transform([[1,0,0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]]))
+print("Should we say goodbye to that customer?")
+print("No" if prediction >= .5  else "Yes")
